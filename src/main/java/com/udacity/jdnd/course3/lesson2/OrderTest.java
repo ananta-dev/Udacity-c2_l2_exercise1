04642d66c6d8a5e58517e5afe4ea6473f08ca7e6
Juanjo Guirao <juan.jose.guirao@gmail.com>
2020-02-08 20:54:36 +0000

    Reading Order Correctly.

--- src/main/java/com/udacity/jdnd/course3/lesson2/OrderTest.java
+++ src/main/java/com/udacity/jdnd/course3/lesson2/OrderTest.java
@@ -18,31 +18,63 @@ public class OrderTest {
 
     public static void main(String[] args) {
         // STEP 1: Create a factory for the persistence unit
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
 
         // STEP 2: Create an EntityManager
+        EntityManager em = factory.createEntityManager();
 
         // STEP 3: Start a transaction
+        em.getTransaction().begin();
 
         // STEP 4: Create an order (entity is in Transient state)
+        Order order = new Order();
+        order.setCustomerName("John Doe");
+        order.setCustomerAddress("324 Winterthur Way, Basingstoke, RG21 7UQ, UK");
+        order.setCreatedTime(Timestamp.valueOf(LocalDateTime.now()));
 
         // create order item1
+        OrderItem orderItem1 = new OrderItem();
+        orderItem1.setOrder(order);
+        orderItem1.setItemName("Parachute");
+        orderItem1.setItemCount(3);
+
 
         // create order item2
+        OrderItem orderItem2 = new OrderItem();
+        orderItem2.setOrder(order);
+        orderItem2.setItemName("Hand glider");
+        orderItem2.setItemCount(3);
+
+        order.setOrderItems(Arrays.asList(orderItem1, orderItem2));
 
         // STEP 5: Persist the order entity
+        em.persist(order);
 
         // NOTE: Order Item is NOT persisted here
+        em.getTransaction().commit();
 
         // entity is persistent now
+        System.err.println("Order ID: " + order.getOrderId());
+        System.err.println("Order Item ID 1: " + orderItem1.getOrderItemId());
+        System.err.println("Order Item ID 2: " + orderItem2.getOrderItemId());
 
+        em.close();
+
+        readOrder(order.getOrderId(), factory);
     }
 
     private static void readOrder(Integer orderId, EntityManagerFactory factory) {
         // STEP 1: Create an EntityManager
+        EntityManager em = factory.createEntityManager();
 
         // STEP 2: use the find() method to load an order
-        // OrderItem is fetched eagerly by using a JOIN
+        Order thisOrder = em.find(Order.class, orderId);
+        System.err.println("Order: " + thisOrder);
 
+        em.close();
+
+        // OrderItem is fetched eagerly by using a JOIN
+        // OrderItem orderItem1 = em.
     }
 
    private static void deleteOrder(Integer orderId, EntityManagerFactory factory) {
